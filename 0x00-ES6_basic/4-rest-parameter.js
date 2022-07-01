a6f1bb94698c0c1b4f0445b08ce0caf250f44b97
nazrawimedhin <nazrawimedhin1@gmail.com>
2022-07-01 17:36:37 +0300

    task 4, rest parameters

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,3 +1,3 @@
-export default function returnHowManyArguments(a, b, ...theArgs) {
-    return arguments.length;
+export default function returnHowManyArguments(...args) {
+    return args.length;
 }
