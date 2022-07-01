9a9de63e45f04f9530eefb6ebacf7c1019e6c01d
nazrawimedhin <nazrawimedhin1@gmail.com>
2022-07-01 11:35:53 +0300

    task 3, default parameters

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,5 @@
+export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
+    expansion1989 =  (typeof(expansion1989) !== "undefined") ? expansion1989 : 89;
+    expansion2019 =  (typeof(expansion1989) !== "undefined") ? expansion1989 : 19;
+    return initialNumber + expansion1989 + expansion2019;
+  }
