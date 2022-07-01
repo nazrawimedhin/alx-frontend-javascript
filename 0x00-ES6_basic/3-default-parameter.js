10a3f54d674918c487a6ea0be057c7c4905cde0d
nazrawimedhin <nazrawimedhin1@gmail.com>
2022-07-01 11:45:53 +0300

    defined default parameter values for the function

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,4 +1,4 @@
-export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
     expansion1989 =  (typeof(expansion1989) !== "undefined") ? expansion1989 : 89;
     expansion2019 =  (typeof(expansion2019) !== "undefined") ? expansion2019 : 19;
     return initialNumber + expansion1989 + expansion2019;
