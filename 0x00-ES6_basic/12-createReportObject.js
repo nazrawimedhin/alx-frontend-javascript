d784d84e99cecd40bccf7842253e84a74fbe962a
nazrawimedhin <nazrawimedhin1@gmail.com>
2022-07-03 09:58:29 +0300

    task 12, creating report object

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,12 +1,10 @@
 export default function createReportObject(employeesList) {
-    ReportObj = {
-        "allEmployees": [employeesList],
-        getNumberOfDepartments: function(employeesList) {
-            for (const value of employeesList) {
-                num_of_departments += 1;
-        }
-        return num_of_departments;
-        }
-    }
-    return ReportObj;
-}
+    return {
+      allEmployees: {
+        ...employeesList,
+      },
+      getNumberOfDepartments(empList) {
+        return Object.keys(empList).length;
+      },
+    };
+  }
