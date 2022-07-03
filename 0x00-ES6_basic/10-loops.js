8ed5b1466d1498785411468735fab5c3bed85e67
nazrawimedhin <nazrawimedhin1@gmail.com>
2022-07-03 15:47:54 +0300

    restarting the project

--- 0x00-ES6_basic/10-loops.js
+++
@@ -1,7 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-    for (const value of array) {
-      array[array.indexof(value)] = appendString + value;
-    }
-  
-    return array;
-  }
