2f998fb094ebec129ab65cb6403a97db335a98f2
nazrawimedhin <nazrawimedhin1@gmail.com>
2022-07-03 08:34:40 +0300

    task 11, creating object

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,7 +1,3 @@
 export default function createEmployeesObject(departmentName, employees) {
-    employeeObj = {
-        departmentName: departmentName,
-        employees: employees
-   }
-   return employeeObj
+   return {[departmentName]: [...employees]}
 }
