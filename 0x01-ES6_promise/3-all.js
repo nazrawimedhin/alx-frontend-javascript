dfcadc0bd8d285c262b5281f7db06adc21525a29
nazrawimedhin <nazrawimedhin1@gmail.com>
2022-07-05 21:52:05 +0300

    code fix : array elements

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,9 +5,9 @@ export default function handleProfileSignup() {
   const user = createUser();
 
   return Promise.all([upld, user]).then((data) => {
-    const body = data[0].body;
-    const fname = data[1].firstname;
-    const lname = data[1].lastname;
+    const [body] = data[0];
+    const fname = data[1].firstName;
+    const lname = data[1].lastName;
     // eslint-disable-next-line
     console.log(`${body} ${fname} ${lname}`);
   }).catch(() => {
