a563bf18fd40bcf66028b2366f47bcbedfc650cd
nazrawimedhin <nazrawimedhin1@gmail.com>
2022-07-06 09:25:44 +0300

    task 3, handling multiple responses

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  let firstName;
-  let lastName;
+  let fName;
+  let lName;
   let body;
 
   const p = uploadPhoto();
@@ -10,10 +10,10 @@ export default function handleProfileSignup() {
 
   return Promise.all([p, u]).then((data) => {
     body = data[0].body;
-    firstName = data[1].firstName;
-    lastName = data[1].lastName;
+    fName = data[1].firstName;
+    lName = data[1].lastName;
     // eslint-disable-next-line
-    console.log(`${body} ${firstName} ${lastName}`);
+    console.log(`${body} ${fName} ${lName}`);
   }).catch(() => {
     // eslint-disable-next-line
     console.log('Signup system offline');
