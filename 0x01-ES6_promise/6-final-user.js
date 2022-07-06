a4c3657d2d62c92c127772951d4d301fb6594b89
nazrawimedhin <nazrawimedhin1@gmail.com>
2022-07-06 18:19:05 +0300

    task 6, ESLint correction

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,5 +1,7 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+/* eslint-disable import/extensions */
+// eslint-disable-next-line import/extensions
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(fisrtName, lastName, fileName) {
   signUpUser(fisrtName, lastName);
