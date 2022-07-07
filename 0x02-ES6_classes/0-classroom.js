28a1816bfd2d2d8acf7cfbdf5a051026d85b767f
nazrawimedhin <nazrawimedhin1@gmail.com>
2022-07-07 17:45:43 +0300

    minor code fix

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,6 @@
 export default class ClassRoom {
   constructor(maxStudentsSize) {
     // eslint-disable-next-line no-underscore-dangle, no-unused-vars
-    const _maxStudentsSize = maxStudentsSize;
+    this._maxStudentsSize = maxStudentsSize;
   }
 }
