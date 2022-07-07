6b83bd626d3d1de596719ce8aa8feeab05a44e98
nazrawimedhin <nazrawimedhin1@gmail.com>
2022-07-07 17:57:27 +0300

    task 1, initializing objects

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/extensions
+import ClassRoom from './0-classroom.js';
+
+export default function initializeRooms() {
+  const array = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  return array;
+}
